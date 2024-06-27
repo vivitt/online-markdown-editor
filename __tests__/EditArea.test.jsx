087b56330fb0918f72af54a4f0c601c098812f2e
Vivi Yanez <viviviyanez@gmail.com>
2024-06-27 15:08:50 +0200

    add edit area test

---
+++ __tests__/EditArea.test.jsx
@@ -0,0 +1,23 @@
+import { cleanup, fireEvent, render, screen } from "@testing-library/react";
+import { afterEach, describe, it, expect, spyOn, vi } from "vitest";
+import userEvent from "@testing-library/user-event";
+import EditArea from "../app/ui/EditArea";
+
+const getItemSpy = vi.spyOn(Storage.prototype, "getItem");
+const setItemSpy = vi.spyOn(Storage.prototype, "setItem");
+const user = userEvent.setup();
+const LOCAL_STORAGE_KEY = "markdown-content";
+
+describe("editor buttons", () => {
+  afterEach(cleanup);
+  afterEach(() => {
+    localStorage.clear();
+    getItemSpy.mockClear();
+    setItemSpy.mockClear();
+  });
+
+  it("image button adds ![add alt text](paste image link)", () => {
+    const { container } = render(<EditArea />);
+    const imgButton = screen.getAllByRole("button", { name: "image" });
+  });
+});
